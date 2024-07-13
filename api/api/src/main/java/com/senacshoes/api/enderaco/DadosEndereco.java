ffa785698006be9b79f162a92e8872b8391099bb
Heitor <full54078@gmail.com>
2024-07-12 21:20:21 -0300

    DadosMedicos versão 1

---
+++ api/api/src/main/java/com/senacshoes/api/enderaco/DadosEndereco.java
@@ -0,0 +1,6 @@
+package com.senacshoes.api.enderaco;
+
+public record DadosEndereco(String logradoudo, String bairro, String cep, String cidade, String uf, String numero, String complemento) {
+
+	
+}
